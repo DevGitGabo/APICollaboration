7c475a978cbfa58402c1c0e7db3c5e060cf3fc45
Gabriel Paiva <gabrielpaiva@gmail.com>
2024-06-08 10:40:09 -0500

    Docutación de la API

--- src/main/java/pe/edu/utp/apicollaboration/controller/UsuarioController.java
+++ src/main/java/pe/edu/utp/apicollaboration/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.utp.apicollaboration.model.entity.Usuario;
+import pe.edu.utp.apicollaboration.model.payload.MensajeResponse;
 import pe.edu.utp.apicollaboration.service.IUsuario;
 
 import java.util.HashMap;
@@ -32,15 +33,16 @@ public class UsuarioController {
 
     @DeleteMapping("/usuario/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        Map<String, Object> response = new HashMap<>();
         try {
             Usuario usuarioDelete = usuarioService.findById(id);
             usuarioService.delete(usuarioDelete);
             return new ResponseEntity<>(usuarioDelete, HttpStatus.NO_CONTENT);
         }catch (DataAccessException exDt) {
-            response.put("mensaje", exDt.getMessage());
-            response.put("mensaje", exDt.getMessage());
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return new ResponseEntity<>(MensajeResponse.builder().
+                    mensaje(exDt.getMessage()).
+                    object(null).
+                    build(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/main/java/pe/edu/utp/apicollaboration/model/dao/CursoDao.java
+++ src/main/java/pe/edu/utp/apicollaboration/model/dao/CursoDao.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.utp.apicollaboration.model.entity.Curso;
 
-@Repository
 public interface CursoDao extends CrudRepository<Curso, Long> {
 }

32b72a4b6e4ac72833b3392ad9219b3b74e48418
Gabriel Paiva <gabrielpaiva@gmail.com>
2024-06-08 19:53:39 -0500

    CORS Intento 2

--- src/main/java/pe/edu/utp/apicollaboration/infra/security/CorsConfig.java
+++ src/main/java/pe/edu/utp/apicollaboration/infra/security/CorsConfig.java
@@ -2,15 +2,19 @@ package pe.edu.utp.apicollaboration.infra.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("/**") // Permitir solicitudes desde este origen
+                .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
